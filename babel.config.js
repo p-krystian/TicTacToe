e3c7b3c1f780c02ee8f31d8e294001fa112b6b5b
Krystian Piątek <108371287+p-krystian@users.noreply.github.com>
2026-03-01 14:02:19 +0100

    Refactor babel.config.js by cleaning up comments
    Removed comments and adjusted module resolver configuration.

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,21 @@
 module.exports = function (api) {
-  // Log to ensure the custom config is being loaded by Metro
   console.log('\n\n🛠️ 🛠️ 🛠️ LOADING ROBUST REGEX BABEL CONFIG 🛠️ 🛠️ 🛠️\n\n');
-  
-  // Temporarily disable cache for testing purposes
   api.cache(false); 
 
   return {
     presets: [
-      // Pass the required polyfill setting directly to the expo preset
       ['babel-preset-expo', { unstable_transformImportMeta: true }]
     ],
     plugins: [
       [
         'module-resolver',
         {
-          root: ['./'],
+          // Usunęliśmy root: ['./'], aby uniknąć konfliktów z plikami wewnętrznymi RN!
           extensions: [
             '.windows.tsx', '.windows.ts', '.windows.js', 
             '.tsx', '.ts', '.jsx', '.js', '.json', '.css'
           ],
           alias: {
-            // Using a regular expression to map anything after @/ to ./src/
             '^@/(.+)': './src/\\1',
           },
         },
